7b320e5d7a8e88eef490eefa2d4417837bbb19c7
Karlis Paulus <pauluskarlis@gmail.com>
2024-02-03 17:58:33 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName + "!");
+
+Console.WriteLine($"Hello, {userName}!");
